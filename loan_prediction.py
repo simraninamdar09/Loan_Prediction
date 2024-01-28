40bef9ea2e443a620e79756257264ffa08954204
Simran Inamdar <125954439+simraninamdar09@users.noreply.github.com>
2024-01-28 20:02:50 +0530

    Update loan_prediction.py

--- loan_prediction.py
+++ loan_prediction.py
@@ -78,9 +78,9 @@ if st.button('Prevention Type'):
     predictions = bag_c.predict(df1)
 
     if predictions.any() == 1:
-         print("Loan approved")
+         prediction_value = "Loan approved"
     else:
-       print("Not Loan Approved")
+       prediction_value = "Not Loan Approved"
     st.title("Loan Status " + str(prediction_value))
 
 
