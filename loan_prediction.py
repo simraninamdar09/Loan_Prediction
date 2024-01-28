a5a789fc8bd8ed0cfbf5221d41d159b96fdc5b08
Simran Inamdar <125954439+simraninamdar09@users.noreply.github.com>
2024-01-28 19:48:26 +0530

    Update loan_prediction.py

--- loan_prediction.py
+++ loan_prediction.py
@@ -75,7 +75,7 @@ if st.button('Prevention Type'):
     }
 
     df1 = pd.DataFrame(df, index=[1])
-    pred = bag1.predict(df1)
+    #pred = bag1.predict(df1)
     pred = pred.reshape(1,-1)
 
     if bag1.predict(pred) == 1:
